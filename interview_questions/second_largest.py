84e76ea5232a66d4f7f4cd0ea045e40b949b3c40
seetharamanrs <seetharaman2632001@gmail.com>
2026-02-19 20:05:58 +0000

    19/02/2026

--- interview_questions/second_largest.py
+++ interview_questions/second_largest.py
@@ -1,12 +1,12 @@
 l=[34,56,78,12,36,-5,-45,2]
+def second_largest():
+    for i in range(len(l)):
+        mini=i
+        for j in range(i+1,len(l),1):
+            mini=j
+        l[i],l[mini]=l[mini],l[i]
 
-for i in range(len(l)):
-    mini=i
-    for j in range(i+1,len(l),1):
-        mini=j
-    l[i],l[mini]=l[mini],l[i]
-
-print(l[-2])
+    print(l[-2])
 
 
 
