1b782a3588c5afd062f8b4c8cfd29f4c43c330bc
seetharamanrs <seetharaman2632001@gmail.com>
2026-03-09 09:13:46 +0530

    Longest_words

--- interview_questions/longest_words.py
+++ interview_questions/longest_words.py
@@ -1,8 +1,17 @@
-m="Python is a powerful programming language"
-n=m.split()
-largest=0
-for i in range(len(n)):
-    if len(n[i])>len(n[largest]):
-        largest=i
-print(n[largest])
-        
+def longest_words(m):
+    n=m.split()
+    largest=0
+    for i in range(len(n)):
+        if len(n[i])>len(n[largest]):
+            largest=i
+    return n[largest]
+
+# m="Python is a powerful programming language"
+while True:
+    try:
+        m = input("Enter the Sentences: ")
+        break
+    except ValueError:
+        print("Please Enter the Character ")
+
+print(longest_words(m))
